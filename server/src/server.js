ccbae594cfb885313cb31ed39644f3043b245560
iphyokafor <sandraifeoma22@gmail.com>
2020-08-04 16:20:15 +0100

    ft: create backend server

---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:4041',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to quick credit!' });
+});
+
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`);
+});
