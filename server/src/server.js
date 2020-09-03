d57e74dc637debd7d090caebeacf46e75f9fa0ba
iphyokafor <sandraifeoma22@gmail.com>
2020-09-03 14:11:32 +0100

    ft:chore-configure database

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import router from './routes/index';
 
 const app = express();
 
@@ -12,11 +13,14 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api', router);
+
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to quick credit!' });
 });
 
-const PORT = process.env.PORT || 4040;
+const PORT = 4040;
+// const PORT = process.env.PORT || 4040;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}.`);
 });
